34c09076d4a08df6cea668f6d5eb57d3ddb5c69d
prasanna <kumarethiraj94@gmail.com>
2018-04-08 18:20:21 +0530

    testing

--- abi/themeforest-13619290-maxi-health-medical-health-html-template/MaxiHealth-HTML-Package/Maxi-Health/js/from.js
+++ abi/themeforest-13619290-maxi-health-medical-health-html-template/MaxiHealth-HTML-Package/Maxi-Health/js/from.js
@@ -1,8 +1,12 @@
-
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
+function validateform(){  
+var name=document.myform.name.value;  
+var tel=document.myform.tel.value;  
+  
+if (name==null || name==""){  
+  alert("Name can't be blank");  
+  return false;  
+}else if(tel.length<10){  
+  alert("Ph no must be at least 10 characters long.");  
+  return false;  
+  }  
+}  
