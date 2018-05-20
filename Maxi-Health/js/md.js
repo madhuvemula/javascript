7d44c6504962def828187214edfd123bf8ec07f4
madhuvemula <vemulamadhusiva@gmail.com>
2018-05-20 17:29:51 +0530

    test 1

--- Maxi-Health/js/md.js
+++ Maxi-Health/js/md.js
@@ -4,11 +4,10 @@
 //       $("#content section:visible").fadeOut();
 //       $(id).fadeIn();
 // });
-$("#content-wrap").slideToggle();
-$("a").on("click", function(e) {
-    e.preventDefault();
-    var sectionID = '#'+ $(this).data("section");
+$("#content-warp").slideToggle();
+$('a').on("click", function(e) {
+    
+    var sectionID = '#' + $(this).data("section");
     $("#content section:visible").slideToggle();
     $(sectionID).show();
 });
-
